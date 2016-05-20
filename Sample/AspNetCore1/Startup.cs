04fb2478789f297827ae390063f582694a8839b8
Rainer Stropek <rainer@software-architects.at>
2016-05-20 08:14:17 +0200

    Change options so that sample does not only listen on localhost

--- Sample/AspNetCore1/Startup.cs
+++ Sample/AspNetCore1/Startup.cs
@@ -97,6 +97,7 @@ namespace AspNetCore1Angular2Intro
         {
             var host = new WebHostBuilder()
               .UseKestrel()
+              .UseUrls("http://*:5000")
               .UseContentRoot(Directory.GetCurrentDirectory())
               .UseIISIntegration()
               .UseStartup<Startup>()
